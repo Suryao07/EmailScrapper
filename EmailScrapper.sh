32430c7c89872176670bd1e47e7ae5bb75265e15
Suryao07 <surya7978252@gmail.com>
2024-01-17 11:26:30 +0530

    EmailScrapper.sh

--- EmailScrapper.sh
+++ EmailScrapper.sh
@@ -1,2 +1,52 @@
 #!/bin/bash
-python3 /home/suryadev/Downloads/EmailScrapper.py
+from bs4 import BeautifulSoup
+import requests
+import requests.exceptions
+import urllib.parse
+from collections import deque
+import re
+
+user_url = str(input("[+]Enter The URL To Scan: "))
+urls = deque([user_url])
+
+scraped_urls = set()
+email = set()
+
+count = 0
+try:
+    while len(urls):
+        count += 1
+        if count == 100:
+            break
+        url = urls.popleft()
+        scraped_urls.add(url)
+
+        parts = urllib.parse.urlsplit(url)
+        base_url = '{0.scheme}://{0.netloc}'.format(parts)
+
+        path = url[:url.rfind('/') + 1] if '/' in parts.path else url
+        print('[%d] Processing %s' % (count, url))
+        try:
+            response = requests.get(url)
+        except (requests.exceptions.MissingSchema, requests.exceptions.ConnectionError):
+            continue
+
+        new_emails = set(re.findall(r'[a-z0-9\. \-+]+@[a-z0-9\. \-+]+\.[a-z]+', response.text, re.I))
+        email.update(new_emails)
+
+        soup = BeautifulSoup(response.text, features="lxml")
+
+        for anchor in soup.find_all("a"):
+            link = anchor.attrs['href'] if 'href' in anchor.attrs else ''
+            if link.startswith('/'):
+                link = base_url + link
+            elif not link.startswith('http'):
+                link = path + link
+            if link not in urls and link not in scraped_urls:
+                urls.append(link)
+
+except KeyboardInterrupt:
+    print('[-] Closing!')
+
+for mail in email:
+    print(mail)
